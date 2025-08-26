438b2d093fa1ed8c99ec3476de6a63d4afaa8cae
marquisalexander <marquis.alexander@outlook.com>
2025-08-26 11:58:20 -0300

    feature: implementacao do jwt

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -17,16 +17,18 @@ export async function authenticate(
   try {
     const authenticateUseCase = makeAuthenticateUseCase();
 
-    await authenticateUseCase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password,
     });
+
+    const token = await reply.jwtSign({}, { sign: { sub: user.id } });
+
+    return reply.status(200).send({ token });
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: err.message });
     }
     return reply.status(500).send();
   }
-
-  return reply.status(200).send();
 }
